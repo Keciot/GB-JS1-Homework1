b82ef31a28db4151cafcfc0eb7049139af9ff173
Keciot <Keciot@gmail.com>
2021-03-07 18:39:47 +0300

    Исправлен конец игры

--- gameCodeFixed.js
+++ gameCodeFixed.js
@@ -2,38 +2,37 @@ var step = 1;
 
 var answerLog = [];
 var i = 0
-if (step <= 2 && step > 0) {
-    while (fetchNextStep()) {
-        fetchNextStep();
-    }
 
-} else {
-    alert('Конец игры(');
+while (fetchNextStep()) {
+    fetchNextStep();
 }
 
+function fetchNextStep() {
+    if (step <= 2 && step > 0) {
 
-function fetchNextStep(step) {
+        if (i < 3) {
+            step = +prompt(steps[i][0] + steps[i][1] + steps[i][2] + 'Что-то еще - Выход из игры');
+            answerLog.push(i + '' + step);
+            i += step;
+            console.log(answerLog);
+            console.log(i);
+            return true;
+        } else if (i == 3 || i == 5) {
+            alert(steps[i][0]);
 
-    if (i < 3) {
-        step = +prompt(steps[i][0] + steps[i][1] + steps[i][2] + '-1 - Выход из игры');
-        answerLog.push(i + '' + step);
-        i += step;
-        console.log(answerLog);
-        console.log(i);
-        return true;
-    } else if (i == 3 || i == 5) {
-        alert(steps[i][0]);
-
-        return false;
-    } else if (i == 4 ) {
-        step = +prompt(steps[4][0] + steps[4][1] + steps[4][2] + '-1 - Выход из игры');
-        answerLog.push(i + '' + step);
-         if (step == 1) {
-             i = 3;
-         } else {
-             i = 5;
-         }
-        return true;
+            return false;
+        } else if (i == 4) {
+            step = +prompt(steps[4][0] + steps[4][1] + steps[4][2] + 'Что-то еще - Выход из игры');
+            answerLog.push(i + '' + step);
+            if (step == 1) {
+                i = 3;
+            } else {
+                i = 5;
+            }
+            return true;
+        }
+    } else {
+        alert('Конец игры(');
     }
 
 }
