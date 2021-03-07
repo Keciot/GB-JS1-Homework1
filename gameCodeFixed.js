ea200d722ffe296d4e97bba547e65ba460522340
Keciot <Keciot@gmail.com>
2021-03-07 18:42:53 +0300

    Исправлен второй алерт

--- gameCodeFixed.js
+++ gameCodeFixed.js
@@ -1,11 +1,8 @@
 var step = 1;
-
 var answerLog = [];
 var i = 0
 
-while (fetchNextStep()) {
-    fetchNextStep();
-}
+fetchNextStep();
 
 function fetchNextStep() {
     if (step <= 2 && step > 0) {
@@ -16,11 +13,10 @@ function fetchNextStep() {
             i += step;
             console.log(answerLog);
             console.log(i);
-            return true;
+            fetchNextStep();
         } else if (i == 3 || i == 5) {
             alert(steps[i][0]);
-
-            return false;
+            return;
         } else if (i == 4) {
             step = +prompt(steps[4][0] + steps[4][1] + steps[4][2] + 'Что-то еще - Выход из игры');
             answerLog.push(i + '' + step);
@@ -29,10 +25,10 @@ function fetchNextStep() {
             } else {
                 i = 5;
             }
-            return true;
+            fetchNextStep();;
         }
     } else {
         alert('Конец игры(');
+        return;
     }
-
 }
