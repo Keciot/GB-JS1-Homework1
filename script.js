50962ecea5849b4cf469ab5abb560e297baf7a8f
Keciot <Keciot@gmail.com>
2021-03-10 00:57:23 +0300

    фигуры расставлены

--- script.js
+++ script.js
@@ -1,12 +1,137 @@
-/* var mat = document.createElement('table');
-var tr, td, i;
-for (i = 0; i < 10; i++) {
-    tr = document.createElement('tr');
-    mat.append(tr);
-    for (j=0; j<10; j++) {
-        td = document.createElement('td');
-        tr.append(td);
-    }
-}
+var figures = [
+    wp1 = {
+        pic: '&#9817;',
+        position: 'A2'
+    },
+    wp2 = {
+        pic: '&#9817;',
+        position: 'B2'
+    },
+    wp3 = {
+        pic: '&#9817;',
+        position: 'C2'
+    },
+    wp4 = {
+        pic: '&#9817;',
+        position: 'D2'
+    },
+    wp5 = {
+        pic: '&#9817;',
+        position: 'E2'
+    },
+    wp6 = {
+        pic: '&#9817;',
+        position: 'F2'
+    },
+    wp7 = {
+        pic: '&#9817;',
+        position: 'G2'
+    },
+    wp8 = {
+        pic: '&#9817;',
+        position: 'H2'
+    },
+    wr1 = {
+        pic: '&#9814;',
+        position: 'A1'
+    },
+    wr2 = {
+        pic: '&#9814;',
+        position: 'H1'
+    },
+    wh1 = {
+        pic: '&#9816;',
+        position: 'B1'
+    },
+    wh2 = {
+        pic: '&#9816;',
+        position: 'G1'
+    },
+    wb1 = {
+        pic: '&#9815;',
+        position: 'C1'
+    },
+    wb2 = {
+        pic: '&#9815;',
+        position: 'F1'
+    },
+    wq2 = {
+        pic: '&#9813;',
+        position: 'D1'
+    },
 
-document.getElementById('field').append(mat); */
+    wk2 = {
+        pic: '&#9812;',
+        position: 'E1'
+    },
+    // черные
+
+    bp1 = {
+        pic: '&#9823;',
+        position: 'A8'
+    },
+    bp2 = {
+        pic: '&#9823;',
+        position: 'B8'
+    },
+    bp3 = {
+        pic: '&#9823;',
+        position: 'C8'
+    },
+    bp4 = {
+        pic: '&#9823;',
+        position: 'D8'
+    },
+    bp5 = {
+        pic: '&#9823;',
+        position: 'E8'
+    },
+    bp6 = {
+        pic: '&#9823;',
+        position: 'F8'
+    },
+    bp7 = {
+        pic: '&#9823;',
+        position: 'G8'
+    },
+    bp8 = {
+        pic: '&#9823;',
+        position: 'H8'
+    },
+    br1 = {
+        pic: '&#9820;',
+        position: 'A7'
+    },
+    br2 = {
+        pic: '&#9820;',
+        position: 'H7'
+    },
+    bh1 = {
+        pic: '&#9822;',
+        position: 'B7'
+    },
+    bh2 = {
+        pic: '&#9822;',
+        position: 'G7'
+    },
+    bb1 = {
+        pic: '&#9821;',
+        position: 'C7'
+    },
+    bb2 = {
+        pic: '&#9821;',
+        position: 'F7'
+    },
+    bq2 = {
+        pic: '&#9819;',
+        position: 'D7'
+    },
+
+    bk2 = {
+        pic: '&#9818;',
+        position: 'E7'
+    }
+
+
+
+]
